6b60a5f5183f8fc1782e1adaa52cab73dfcb1ed3
Jason Dudley <jasondudley@mac.com>
2016-11-09 14:35:13 -0700

    fix shit make shit work

--- public/js/join.js
+++ public/js/join.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
-    var $form = $('#join-form');
-    $form.submit(function(event){
-        $form = $('#join-form');
-        $form.find('.submit').prop('disabled',true);
-        Stripe.card.createToken($form, stripeResponseHandler);
-        console.log($form);
-        return false;
-    })
     $('#submit-form').on('click',function(){
-        console.log('submit button clicked!');
+        var joinForm = $('#join-form');
+        Stripe.card.createToken(joinForm, stripeResponseHandler);
+        joinForm[0].reset();
+        $('.brand').click();
+        return false;
     });
+    function stripeResponseHandler(response) {
+        console.log(response);
+    }
+    // {
+    //   id: "tok_u5dg20Gra", // Token identifier
+    //   card: {...}, // Dictionary of the card used to create the token
+    //   created: 1478722383, // Timestamp of when token was created
+    //   currency: "usd", // Currency that the token was created in
+    //   livemode: false, // Whether this token was created with a live API key
+    //   object: "token", // Type of object, always "token"
+    //   used: false // Whether this token has been used
+    // }
 });
