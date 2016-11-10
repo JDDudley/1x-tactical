8426c279b569e52a7ba50708eef6b820eb6b32b2
Jason Dudley <jasondudley@mac.com>
2016-11-10 14:15:36 -0700

    new payment model structure

--- server-assets/models/payment-model.js
+++ server-assets/models/payment-model.js
@@ -3,12 +3,15 @@ let stripe = require("stripe")("sk_test_tlI06RUdE1tVOzucTg99mHne");
 function create(body) {
     console.log('creating membership charge...');
     var token = body.stripeToken;
-    var charge = stripe.charges.create({
-        amount: 9900,
-        currency: "usd",
-        source: token,
-        description: "1X Tactical Yearly Membership"
-    }, function (err, charge) {
+    console.log('Stripe Token: ' + token);
+    var chargeObj = {
+        "amount": 9900,
+        "currency": "usd",
+        "source": token,
+        "description": "1X Tactical Yearly Membership"
+    }
+    console.log('Charge Object:' + chargeObj);
+    var charge = stripe.charges.create(chargeObj, function (err, charge) {
         if (err && err.type == 'StripeCardError') {
             return { "error": "The card has been declined." };
         }
