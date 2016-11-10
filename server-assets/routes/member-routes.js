affa64e377f9ba422d93eb61109a8248ea2979ce
Jason Dudley <jasondudley@mac.com>
2016-11-10 09:20:25 -0700

    add payment route/model back in

--- server-assets/routes/member-routes.js
+++ server-assets/routes/member-routes.js
@@ -5,33 +5,33 @@ module.exports.mountPath = '/membership'
 module.exports.router = router;
 
 router.route('/:id?')
-    .get(function(req, res, next){ 
-    Membership.getAll(function(data){
-        if(data.stack){
-            return next(data)
-        }
-       res.send(data)
+    .get(function (req, res, next) {
+        Membership.getAll(function (data) {
+            if (data.stack) {
+                return next(data)
+            }
+            res.send(data)
+        })
+
     })
-    
-    })
-    .post(function(req, res, next){
-        Membership.create(req.body.member, function(member){
-            if(member.stack){
+    .post(function (req, res, next) {
+        Membership.create(req.body.member, function (member) {
+            if (member.stack) {
                 return next(member)
             }
-             res.send(member)
+            res.send(member)
         })
     })
     .put(function (req, res, next) {
-    Membership.updateMember(req.params.id, req.body.member, function(member){
-      if(member.stack) { return next(member) }
-        res.send(member)
+        Membership.updateMember(req.params.id, req.body.member, function (member) {
+            if (member.stack) { return next(member) }
+            res.send(member)
+        })
     })
-  }) 
-  .delete(function(req, res, next){
-   Membership.removeMember(req.params.id, function(member){
-      if(member.stack) { return next(member) }
-      res.send ({message: "Member Successfully Deleted. Arm Up, Train Up, Stand Up"}) 
+    .delete(function (req, res, next) {
+        Membership.removeMember(req.params.id, function (member) {
+            if (member.stack) { return next(member) }
+            res.send({ message: "Member Successfully Deleted. Arm Up, Train Up, Stand Up" })
+        })
     })
-})
 
---
+++ server-assets/routes/payment-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const Payment = require('../models/payment-model');
+
+module.exports.mountPath = '/payment'
+module.exports.router = router;
+
+router.route('/membership')
+    .post(function (req, res, next) {
+        Membership.create(req.body, function (payment) {
+            if (payment.stack) {
+                return next(payment)
+            }
+            res.send(payment)
+        })
+    })
