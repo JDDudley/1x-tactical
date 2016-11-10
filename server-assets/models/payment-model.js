0fb863f39c8121f1b33c34fc7677c125c1a799d2
Jason Dudley <jasondudley@mac.com>
2016-11-10 09:05:46 -0700

    test without payment model

--- server-assets/models/payment-model.js
+++ server-assets/models/payment-model.js
@@ -5,20 +5,20 @@ let stripe = require("stripe")("sk_test_tlI06RUdE1tVOzucTg99mHne");
 // Get the credit card details submitted by the form
 var token = request.body.stripeToken; // Using Express
 
-function create(req) {
-    var token = req.body.stripeToken;
-    var charge = stripe.charges.create({
-        amount: 9900,
-        currency: "usd",
-        source: token,
-        description: "1X Tactical Yearly Membership"
-    }, function (err, charge) {
-        if (err && err.type == 'StripeCardError') {
-            return { "error": "The card has been declined." };
-        }
-    });
-}
+// function create(req) {
+//     var token = req.body.stripeToken;
+//     var charge = stripe.charges.create({
+//         amount: 9900,
+//         currency: "usd",
+//         source: token,
+//         description: "1X Tactical Yearly Membership"
+//     }, function (err, charge) {
+//         if (err && err.type == 'StripeCardError') {
+//             return { "error": "The card has been declined." };
+//         }
+//     })
+// };
 
-module.exports{
-    create
-}
+// module.exports{
+//     create
+// }
