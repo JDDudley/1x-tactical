543d95f99125a55d30ddb699836d491b84f8e154
Jason Dudley <jason@jddudley.com>
2016-12-07 16:50:58 -0700

    Temp Disable Firebase Auth

--- server-assets/models/data-adapter.js
+++ server-assets/models/data-adapter.js
@@ -18,16 +18,16 @@ formatQuery= function(query){
 DS.registerAdapter('firebase', fbAdapter, { default: true })
 
 //firebase attempts - add in server-side authorization
-let config = {
-  apiKey: 'AIzaSyDSo9P5PjcuV5UsFH2wwvCkI2PNqdUFGm8',
-  authDomain: 'x-tactical.firebaseapp.com',
-  databaseURL: 'https://x-tactical.firebaseio.com',
-  storageBucket: 'x-tactical.appspot.com',
-  messagingSenderId: '801571924462'
-}
+// let config = {
+//   apiKey: 'AIzaSyDSo9P5PjcuV5UsFH2wwvCkI2PNqdUFGm8',
+//   authDomain: 'x-tactical.firebaseapp.com',
+//   databaseURL: 'https://x-tactical.firebaseio.com',
+//   storageBucket: 'x-tactical.appspot.com',
+//   messagingSenderId: '801571924462'
+// }
 
-const firebaseApp = firebase.initializeApp(config)
-const authStore = new FirebaseAuthStore(firebaseApp)
+// const firebaseApp = firebase.initializeApp(config)
+// const authStore = new FirebaseAuthStore(firebaseApp)
 
 //end firebase tests
 
