1fa2c3c3b78f824994348adc7cebfb2b4ab97e01
Jason Dudley <jason@jddudley.com>
2016-12-07 13:50:21 -0700

    Break off Navbar

---
+++ public/app/components/navbar/navbar.js
@@ -0,0 +1,9 @@
+angular.module('1x')
+  .component('navbar', {
+    templateUrl: '/app/components/navbar/navbar.html',
+    controller($scope) {
+      let $ctrl = this
+
+    },
+    controllerAs: 'nc'
+  })
