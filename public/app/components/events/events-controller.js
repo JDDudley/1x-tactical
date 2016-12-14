338d4738762176c871b119a7364fc6462413fe95
runnerguy55 <michaelhoffmanj@gmail.com>
2016-12-14 12:38:19 -0700

    start branch to connect armory and events cart

--- public/app/components/events/events-controller.js
+++ public/app/components/events/events-controller.js
@@ -6,15 +6,23 @@
             controller: EventsController
         })
 
-EventsController.$inject=['EventsService']
-    function EventsController(ES) {
+EventsController.$inject=['EventsService', 'CartController', 'CartService']
+    function EventsController(ES, CC, CS) {
         var ec = this;
         ec.events=[];
 
+        ec.$onInit=function(){
         ES.getEvents((events)=>{
             console.log(events)
             ec.events=events.data
-        });
+        })
+        }
+
+        ec.addToCart=(event)=>{
+            debugger 
+            CC.addProductToCart(event)
+            alert('Event Added To Cart.')
+        }
         
     }
 
