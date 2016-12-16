b9d174af66d07c1318369af2eeede75be64b455c
runnerguy55 <michaelhoffmanj@gmail.com>
2016-12-15 21:39:55 -0700

    local storage cart.

--- public/app/components/events/events-controller.js
+++ public/app/components/events/events-controller.js
@@ -6,16 +6,29 @@
             controller: EventsController
         })
 
-EventsController.$inject=['EventsService']
-    function EventsController(ES) {
+    EventsController.$inject = ['EventsService', 'CheckoutService']
+    function EventsController(ES, CS) {
         var ec = this;
-        ec.events=[];
+        ec.events = [];
 
-        ES.getEvents((events)=>{
+        ES.getEvents((events) => {
             console.log(events)
-            ec.events=events.data
+            ec.events = events.data
         });
-        
+
+        ec.addToCart = (event) => {
+            debugger
+            console.log(event)
+            CS.addToCart(event)
+            console.log('adding event to cart..')
+        }
+
+    ec.removeFromCart=(id)=>{
+        debugger 
+        CS.removeFromCart(id)
+        console.log(event.name + " has been removed from your shopping cart.")
+    }
+
     }
 
 })(); 
