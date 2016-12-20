69deb756894d91dac8b7d5b5e8741e399dfe5c8a
Chad Rohr <chadrohr@yahoo.com>
2016-12-20 14:41:21 -0700

    bringing down events

--- public/app/components/events/events-controller.js
+++ public/app/components/events/events-controller.js
@@ -6,19 +6,27 @@
             controller: EventsController
         })
 
-EventsController.$inject=['EventsService', 'CartController', 'CartService']
-    function EventsController(ES, CC, CS) {
+    EventsController.$inject = ['EventsService', 'CheckoutService']
+    function EventsController(ES, CS) {
         var ec = this;
-        ec.events=[];
+        ec.events = [];
 
-        ES.getEvents((events)=>{
+        ES.getEvents((events) => {
             console.log(events)
-            ec.events=events.data
+            ec.events = events.data
         });
-        
-          ec.addToCart=(event)=>{
-            CC.addProductToCart(event)
+
+        ec.addToCart = (event) => {
+            console.log(event)
+            CS.addToCart(event)
+            console.log('adding event to cart..')
         }
+
+    ec.removeFromCart=(id)=>{
+        CS.removeFromCart(id)
+        console.log(event.name + " has been removed from your shopping cart.")
+    }
+
     }
 
 })(); 
