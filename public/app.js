3a1f864ba95d9af0691ed573f58a2aff09bb63e7
JD Dudley <jason@jddudley.com>
2016-12-20 16:31:11 -0700

    Recommit Merge

--- public/app.js
+++ public/app.js
@@ -1,17 +1,9 @@
 (() => {
 
 
-<<<<<<< HEAD
-    var app = angular.module('1x', [
-        'ui.router',
-        // 'ng-firebase-auth',
-        'ngSanitize'
-    ]);
-=======
 var app = angular.module('1x', [
     'ui.router',
     'ngSanitize',
-    ]); 
->>>>>>> 186176ae272c9a2f9a470011282f27e8d48c2243
+    ]); 6176ae272c9a2f9a470011282f27e8d48c2243
 
 })();
--- public/app/utils/auth-component.js
+++ public/app/utils/auth-component.js
@@ -1,124 +1,124 @@
-(function() {
+// (function() {
 
-    angular.module('ng-firebase-auth', [])
-        .component('authComponent', {
-            templateUrl: 'app/components/auth/auth.html',
-            controller: AuthController
-        })
-        .factory('User', function(DS) {
+//     angular.module('ng-firebase-auth', [])
+//         .component('authComponent', {
+//             templateUrl: 'app/components/auth/auth.html',
+//             controller: AuthController
+//         })
+//         .factory('User', function(DS) {
 
 						
-            return DS.defineResource({
-                name: 'user',
-                endpoint: 'users',
-								relations: {
-									hasMany: {
-										keep: {
-											localField: 'keeps',
-											localKey: 'userId'
-										}
-									}
-								}
-            });
+//             return DS.defineResource({
+//                 name: 'user',
+//                 endpoint: 'users',
+// 								relations: {
+// 									hasMany: {
+// 										keep: {
+// 											localField: 'keeps',
+// 											localKey: 'userId'
+// 										}
+// 									}
+// 								}
+//             });
 
-        })
-        .factory('AuthService', function(DSFirebaseAdapter, User) {
-            var db = DSFirebaseAdapter.ref;
-            var _member = {}
-            return {
-                register: register,
-                login: login,
-                logout: function() {
-                    db.unauth();
-                    _member = {};
-                },
-                getMember: authMember,
-                getAuth: function() {
-                    return db.getAuth()
-                }
-            }
+//         })
+//         .factory('AuthService', function(DSFirebaseAdapter, User) {
+//             var db = DSFirebaseAdapter.ref;
+//             var _member = {}
+//             return {
+//                 register: register,
+//                 login: login,
+//                 logout: function() {
+//                     db.unauth();
+//                     _member = {};
+//                 },
+//                 getMember: authMember,
+//                 getAuth: function() {
+//                     return db.getAuth()
+//                 }
+//             }
             
-            function authMember() {
-                var authData = db.getAuth();
-                if (!authData) {
-                    return false;
-                }
-                setMember(authData.uid);
-                return _member
-            }
+//             function authMember() {
+//                 var authData = db.getAuth();
+//                 if (!authData) {
+//                     return false;
+//                 }
+//                 setMember(authData.uid);
+//                 return _member
+//             }
 
-            function setMember(id) {
-                return User.find(id).then(function(member) {
-                    Object.keys(member).forEach(function(k) {
-                        _member[k] = member[k]
-                    })
-                })
-            }
+//             function setMember(id) {
+//                 return User.find(id).then(function(member) {
+//                     Object.keys(member).forEach(function(k) {
+//                         _member[k] = member[k]
+//                     })
+//                 })
+//             }
 
-            function createUser(authData, user) {
-                var member = {
-                    id: authData.uid,
-                    username: user.username,
-                    email: user.email,
-                    created: Date.now()
-                }
-                User.create(member);
-            }
+//             function createUser(authData, user) {
+//                 var member = {
+//                     id: authData.uid,
+//                     username: user.username,
+//                     email: user.email,
+//                     created: Date.now()
+//                 }
+//                 User.create(member);
+//             }
 
-            function login(user, cb) {
-                db.authWithPassword(user, function(err, authData) {
-                    if (err) {
-                        return cb(null, err)
-                    }
-                    return cb(authMember());
-                })
-            }
-            function register(user, cb) {
-                db.createUser(user, function(err, authData) {
-                    if (err) {
-                        return cb(null, err)
-                    }
-                    createUser(authData, user);
-                    login(user, cb)
-                });
-            }
-        })
+//             function login(user, cb) {
+//                 db.authWithPassword(user, function(err, authData) {
+//                     if (err) {
+//                         return cb(null, err)
+//                     }
+//                     return cb(authMember());
+//                 })
+//             }
+//             function register(user, cb) {
+//                 db.createUser(user, function(err, authData) {
+//                     if (err) {
+//                         return cb(null, err)
+//                     }
+//                     createUser(authData, user);
+//                     login(user, cb)
+//                 });
+//             }
+//         })
 
-    function AuthController($scope, $state, AuthService) {
-        var ac = this;
-        ac.member = AuthService.getMember();
-        ac.login = function() {
-            clearErr();
-            AuthService.login(ac.auth, handleDBResponse);
-        };
+//     function AuthController($scope, $state, AuthService) {
+//         var ac = this;
+//         ac.member = AuthService.getMember();
+//         ac.login = function() {
+//             clearErr();
+//             AuthService.login(ac.auth, handleDBResponse);
+//         };
 
-        ac.register = function() {
-            clearErr();
-            AuthService.register(ac.auth, handleDBResponse);
-        };
+//         ac.register = function() {
+//             clearErr();
+//             AuthService.register(ac.auth, handleDBResponse);
+//         };
 
-        ac.logout = function() {
-            clearErr();
-            AuthService.logout();
-            ac.member = {};
-            $state.go('home')
-        };
-        function update() {
-            $scope.$evalAsync();
-        }
-        function clearErr() {
-            ac.authErr = '';
-        }
+//         ac.logout = function() {
+//             clearErr();
+//             AuthService.logout();
+//             ac.member = {};
+//             $state.go('home')
+//         };
+//         function update() {
+//             $scope.$evalAsync();
+//         }
+//         function clearErr() {
+//             ac.authErr = '';
+//         }
 
-        function handleDBResponse(member, err) {
-            if (member) {
-                ac.member = member;
-                ac.activeView = ''
-            } 
-            if(err) {
-                ac.error = err.message;
-            }
-            update()
-        }
-    }
-} ())
+//         function handleDBResponse(member, err) {
+//             if (member) {
+//                 ac.member = member;
+//                 ac.activeView = ''
+//             } 
+//             if(err) {
+//                 ac.error = err.message;
+//             }
+//             update()
+//         }
+//     }
+// } ())
