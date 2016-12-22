33edb89f609ef5eca337eac206a5387107113b70
runnerguy55 <michaelhoffmanj@gmail.com>
2016-12-22 16:04:52 -0700

    init

---
+++ webpackconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devtool: 'inline-sourcemap',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+module: {
+  loaders: [
+    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  ]
+}
+};
